35b9e5f24200f642af8382afe1b057922201e1f6
Manuelscholar <manu.maduapera@gmail.com>
2022-01-21 16:23:29 +0200

    Dice app with user authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,11 @@ class HomeController < ApplicationController
   end
 
   def dice
+      if !user_signed_in?
+        redirect_to new_user_session_path
+      else
     @random_dice1_result = "dice#{Die.new(6).roll}"
     @random_dice2_result = "dice#{Die.new(6).roll}"
-    
+      end
   end
 end
